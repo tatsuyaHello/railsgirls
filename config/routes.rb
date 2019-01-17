cc5b3e2b958ebe6fa8b1015a6ff99799c8138766
tatsuyaHello <vagrant@localhost.localdomain>
2019-01-17 10:12:28 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'pages/info'
+  resources :ideas
+
+  #root to: redirect('/ideas')  書き方の違いは思想の違い
+  root to: "ideas#index"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
